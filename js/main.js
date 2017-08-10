feec24899b95d6b350d5e12078609a947e521103
Dima Zatyliuk <dimazdoks@gmail.com>
2017-08-10 12:36:32 +0300

    Init

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var tShirt = document.getElementsByTagName('img')[0];
+
+document.getElementById('black').onclick = function() {
+	tShirt.src = 'css/img/t-shirt2.png';
+}
+document.getElementById('white').onclick = function() {
+	tShirt.src = 'css/img/t-shirt1.png';
+}
